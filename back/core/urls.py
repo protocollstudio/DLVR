883650e03fb20665ccbb10610288f4d839bc0790
Antharia <artenischloria@gmail.com>
2022-06-07 09:45:42 +0000

    Delete Django track list view

--- back/core/urls.py
+++ back/core/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/tracks/", include("api.urls")),
-    path("", include("tracks.urls")),
 ]
