3dddf1f685590c30afe683cebfa8bdcfe14dc6ff
Antharia <artenischloria@gmail.com>
2022-06-09 14:54:01 +0000

    Add likes and dislikes to tracks

--- back/tracks/models.py
+++ back/tracks/models.py
@@ -6,6 +6,8 @@ class Track(models.Model):
     path = models.CharField(max_length=250, null=True, blank=True)
     artwork = models.ImageField(null=True, blank=True)
     audio_file = models.FileField(null=True, blank=True)
+    likes = models.IntegerField(null=True, blank=True, default=0)
+    dislikes = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return self.title
