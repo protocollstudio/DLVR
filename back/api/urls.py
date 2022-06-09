2caa22dab6f717373edb2db78b8734b365a85dd1
Antharia <artenischloria@gmail.com>
2022-06-09 23:49:47 +0000

    Add like/dislike route

--- back/api/urls.py
+++ back/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TrackAPIView, TrackDetail
+from .views import TrackAPIView, TrackDetail, add_like, add_dislike
 
 
 urlpatterns = [
         path("", TrackAPIView.as_view()),
         path("<int:pk>/", TrackDetail.as_view()),
+        path("<int:pk>/like", add_like),
+        path("<int:pk>/dislike", add_dislike),
         ]
