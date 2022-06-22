d9fc7255f3b7c905afd937ce3f3b994641f88caf
Antharia <artenischloria@gmail.com>
2022-06-22 15:38:50 +0000

    Add UUID to tracks

--- back/tracks/models.py
+++ back/tracks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import uuid
 
 class Track(models.Model):
+    ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=250)
     url = models.URLField(max_length=250)
     path = models.CharField(max_length=250, null=True, blank=True)
